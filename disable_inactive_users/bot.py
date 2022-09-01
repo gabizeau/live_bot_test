7edac752863ede5ead21b25ce7a5f7be9f75d87e
gbizeau <gbizeau@gmail.com>
2022-09-01 14:04:18 -0300

    try Except catch on Data:

--- disable_inactive_users/bot.py
+++ disable_inactive_users/bot.py
@@ -51,31 +51,35 @@ def run(ctx):
     n_days_epoch = int(time.mktime(time.strptime(new_days_ago, pattern)))
 
     if data:
-        logging.info('Checking for users past {0} Days last login'.format(days))
-        for row in data['SonraiUsers']['items']:
+        try:
+            logging.info('Checking for users past {0} Days last login'.format(days))
+            for row in data['SonraiUsers']['items']:
 
-            # Check our never_disable list
-            if row['email'] in never_disable:
-                continue
+                # Check our never_disable list
+                if row['email'] in never_disable:
+                    continue
 
-            # Check if we should remove a Sonrai Security user
-            if '@sonraisecurity' in row['email'] and keep_sonrai_security_users:
-                continue
+                # Check if we should remove a Sonrai Security user
+                if '@sonraisecurity' in row['email'] and keep_sonrai_security_users:
+                    continue
 
-            # Check if the users last login exceeds required timeframe: date - n_days_epoch
-            if int(row['lastLogin']) > int(n_days_epoch):
-                continue
+                # Check if the users last login exceeds required timeframe: date - n_days_epoch
+                if int(row['lastLogin']) > int(n_days_epoch):
+                    continue
 
-            logging.info('Disabling User: {0} ({1})'.format(row['name'], row['email']))
+                logging.info('Disabling User: {0} ({1})'.format(row['name'], row['email']))
 
-            try:
-                mutation_variables = {"updateGuy": {"srn": row['srn'], "isActive": "false"}}
-                return_value = graphql_client.query(gql['updateSonraiGuy.gql'], mutation_variables)
-            except Exception as e:
-                return_value = None
-                logging.info('Something went wrong disabling User: {0} ({1}) - {2}'.format(row['name'], row['email'], e))
+                try:
+                    mutation_variables = {"updateGuy": {"srn": row['srn'], "isActive": "false"}}
+                    return_value = graphql_client.query(gql['updateSonraiGuy.gql'], mutation_variables)
+                except Exception as e:
+                    return_value = None
+                    logging.info('Something went wrong disabling User: {0} ({1}) - {2}'.format(row['name'], row['email'], e))
 
-            if return_value:
-                logging.info('Disabled User: {0} ({1})'.format(return_value['UpdateSonraiUsers']['items'][0]['name'], return_value['UpdateSonraiUsers']['items'][0]['email']))
+                if return_value:
+                    logging.info('Disabled User: {0} ({1})'.format(return_value['UpdateSonraiUsers']['items'][0]['name'], return_value['UpdateSonraiUsers']['items'][0]['email']))
 
-        logging.info('Users iteration completed')
+            logging.info('Users iteration completed')
+
+        except Exception as e:
+            logging.info("General Error: " + repr(e))
